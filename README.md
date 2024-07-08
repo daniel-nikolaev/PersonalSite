aab26398ef1c84b5c63c65320323ce2162abcde0
Daniel Nikolaev <danielnik2016@gmail.com>
2024-07-07 23:40:15 -0400

    calm luh update (except resume)

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ CodeBase for my Personal Website Portfolio
 
 
 &nbsp; - whitespace
+
+
+https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_loader5
---
+++ js/topnav.js
@@ -0,0 +1,8 @@
+function topNav() {
+    var x = document.getElementById("Navbar");
+    if (x.className === "navbar") {
+    x.className += " responsive";
+    } else {
+    x.className = "navbar";
+    }
+}
